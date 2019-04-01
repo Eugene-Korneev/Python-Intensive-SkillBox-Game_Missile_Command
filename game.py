e2b5b20a9f44908f89fc7ef6f4b0339dee3a297c
Eugene Korneev <j.korney@gmail.com>
2019-04-01 11:34:23 +0200

    + Missile interception

--- game.py
+++ game.py
@@ -60,10 +60,29 @@ def move_missile(missiles):
                 missile_info['state'] = 'dead'
             else:
                 missile.shapesize(missile_info['radius'])
+        elif state == 'dead':
+            missile.clear()
+            missile.hideturtle()
 
     dead_missiles = [info for info in missiles if info['state'] == 'dead']
     for dead in dead_missiles:
         missiles.remove(dead)
+
+
+def check_enemy_count():
+    if len(enemy_missiles) < ENEMY_COUNT:
+        fire_enemy_missile()
+
+
+def check_interceptions():
+    for our_info in our_missiles:
+        if our_info['state'] != 'explode':
+            continue
+        our_missile = our_info['missile']
+        for enemy_info in enemy_missiles:
+            enemy_missile = enemy_info['missile']
+            if enemy_missile.distance(our_missile.xcor(), our_missile.ycor()) < 20:
+                enemy_info['state'] = 'dead'
 
 
 window.onclick(fire_missile)
@@ -71,11 +90,10 @@ window.onclick(fire_missile)
 our_missiles = []
 enemy_missiles = []
 
+
 while True:
     window.update()
-
-    if len(enemy_missiles) < ENEMY_COUNT:
-        fire_enemy_missile()
-
+    check_enemy_count()
+    check_interceptions()
     move_missile(missiles=our_missiles)
     move_missile(missiles=enemy_missiles)
